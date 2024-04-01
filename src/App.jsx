9a74420883aaf5090eca2ed020a380500572a30c
Vivek Bhawsar <138601237+ValtechInnovations@users.noreply.github.com>
2024-04-01 12:12:43 +0530

    student detail component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
-import Home from './views/home';
+import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import './App.css'
+
+import { Home } from './views/home';
+import Studentdetail from './views/Studentdetail';
+
+function StudentDetailWrapper() {
+  const { stuId } = useParams(); // Extract stuId from URL parameters
+  return <Studentdetail stuId={stuId} />; // Pass stuId as a prop to Studentdetail component
+}
 
 function App() {
   return (
-    <BrowserRouter> {/* Wrap your application with BrowserRouter */}
-      <Home />
-    </BrowserRouter>
+    // <BrowserRouter> {/* Wrap your application with BrowserRouter */}
+    // <Home />
+    // <h1>Hello</h1>
+    // </BrowserRouter>
+
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/account/accomplishments/verify/:stuId" element={<StudentDetailWrapper />} />
+      </Routes>
+    </Router>
   );
 }
 
